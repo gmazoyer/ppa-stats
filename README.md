8f92a9ead4806cf19cca4b2007c2016ae1f36c64
Guillaume Mazoyer <respawneral@gmail.com>
2013-08-17 00:13:15 +0200

    Add Python script and update README.md.

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-ppa-stats
-=========
+PPA Statistics
+==============
 
-Show statistics for a given PPA
+Show statistics for a given PPA.
+
+Requirements
+------------
+
+  * launchpadlib --> https://help.launchpad.net/API/launchpadlib
+  * texttable    --> https://pypi.python.org/pypi/texttable/
+
+What is it?
+-----------
+
+This code is based on the one made by Sam Hewitt.
+http://www.snwh.org/files/generic-ppa-logging-script
+
+It was modified to:
+
+  * Use the standard output of the shell instead of logging everything in a
+    file.
+  * Be more flexible when adding Ubuntu versions and architectures.
+  * Use beautiful ASCII art tables.
+  * Be more easy to maintain.
+  * Anything more?
---
+++ ppa-stats.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+#
+# Authors:
+#   Sam Hewitt <hewittsamuel@gmail.com>
+#   Guillaume Mazoyer <gmazoyer@gravitons.in>
+#
+
+import sys, os
+import time, datetime
+from texttable import Texttable
+from launchpadlib.launchpad import Launchpad
+
+# For reference: "ppa:ownername/archivename" or
+# "https://launchpad.net/~ownername/+archive/archive-name"
+
+# Name of the owner of the ppa you would like the stats for.
+PPAOWNER = 'java-gnome'
+# Name of the PPA you would like the stats for.
+PPANAME = 'ppa'
+# System CPU architecture you would like the stats for.
+ARCHS = [ 'i386', 'amd64' ]
+# Versions of Ubuntu to check.
+VERSIONS = [ 'precise', 'quantal', 'raring' ]
+
+# Login into Launchpad Anoymously
+lp_login = Launchpad.login_anonymously('ppastats', 'edge',
+    "~/.launchpadlib/cache/", version='devel')
+# PPA owner
+owner = lp_login.people[PPAOWNER]
+# PPA name
+archive = owner.getPPAByName(name=PPANAME)
+
+# Base URL to Launchpad API
+base_url = 'https://api.launchpad.net/devel/ubuntu/'
+
+# Print heading
+print 'Download stats for ' + PPAOWNER + ' PPA'  
+print '----------------------------------------------'
+
+# For each version
+for version in VERSIONS:
+    print ''
+    print 'Packages for ' + version
+
+    result = [ [ 'Package', 'Version', 'Arch', 'Count' ] ]
+
+    # For each architecture
+    for arch in ARCHS:
+        url_to_check = base_url + version + '/' + arch
+
+        for individual_archive in archive.getPublishedBinaries(
+            status='Published',distro_arch_series=url_to_check):
+            # Get download count
+            count = individual_archive.getDownloadCount()
+            # Get the package version
+            packagevers='~'.join(individual_archive.binary_package_version.\
+                split('~')[:-1])
+
+            result.append(
+                [
+                    individual_archive.binary_package_name,
+                    individual_archive.binary_package_version,
+                    arch,
+                    str(individual_archive.getDownloadCount())
+                ]
+            )
+
+    # Show the result in a beautiful table
+    table = Texttable()
+    table.set_cols_dtype([ 't', 't', 't', 'i' ])
+    table.set_cols_align([ 'l', 'r', 'r', 'r' ])
+    table.add_rows(result)
+    print table.draw()
